0986b079ff717b31439a7f4bf57875f46773c584
Sameer <Sameer8406023924@gmail.com>
2024-10-06 14:01:56 +0530

    commit

--- src/resverseString/ReverseString.java
+++ src/resverseString/ReverseString.java
@@ -4,16 +4,9 @@ public class ReverseString {
 
 	public static void main(String[] args) {
  
-		String a = "Sameer";
-		 String b= "";
-		 
-		 for(int i=a.length()-1;i>=0;i--)
-		 {
-			 b=b+a.charAt(i);
-			
-		 }
-		 System.out.println("reverse string:"+ b);
-		
+		String str = "my name is sameer";
+	    	        long count = str.chars().filter(ch -> ch == 'e').count();
+	    	        System.out.println("Total count of 'e': " + count);
 		 }
 	}
 
